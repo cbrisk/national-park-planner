2ada196b6a85e8642a5f4bf1e2eb57b301001dc3
Chaim Brisk <cbrisk99@gmail.com>
2020-12-31 10:50:52 -0800

    added minor visual adjustment

--- client/pages/itinerary-list.jsx
+++ client/pages/itinerary-list.jsx
@@ -24,13 +24,13 @@ export default class ItineraryList extends React.Component {
       });
   }
 
-  render () {
+  render() {
     const spinner = this.state.isLoading ? 'spinner-border blue' : 'spinner-border blue d-none';
     const className = this.state.isLoading ? 'park-info py-3 d-none' : 'park-info py-3';
     return (
       <main className="light-blue pb-3">
         <a href="#"><i className="fas fa-home home-icon medium-blue m-3"></i></a>
-        <div className="d-flex justify-content-center py-3 mx-4 mb-3">
+        <div className="d-flex justify-content-center py-3 mx-4">
           <h3 className="blue itinerary-title">Itineraries</h3>
         </div>
         <div className="d-flex justify-content-center">
@@ -44,7 +44,9 @@ export default class ItineraryList extends React.Component {
                   return (
                     <li className="list-group-item itinerary-item d-flex justify-content-between" key={index}>
                       {park.parkName}
-                      <a href={`#itinerariesById?id=${park.itineraryId}`}><i className="fas fa-chevron-right blue"></i></a>
+                      <div className="d-flex align-items-center">
+                        <a href={`#itinerariesById?id=${park.itineraryId}`}><i className="fas fa-chevron-right blue"></i></a>
+                      </div>
                     </li>
                   );
                 })
